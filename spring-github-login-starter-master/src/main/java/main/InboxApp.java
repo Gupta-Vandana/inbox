99d6327af91666b7921a89f020ab2499180fab15
Vandana Gupta <vandanagupta196@gmail.com>
2023-02-04 18:01:48 +0530

    added functionality for email extraction

--- spring-github-login-starter-master/src/main/java/main/InboxApp.java
+++ spring-github-login-starter-master/src/main/java/main/InboxApp.java
@@ -1,19 +1,20 @@
 package main;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import main.Model.EmailListItem;
+import main.Model.EmailListItemKey;
 import main.Model.Folder;
 import main.Repository.FolderRepository;
+import main.Repository.EmailListItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class InboxApp {
@@ -29,13 +30,33 @@ public class InboxApp {
             builder.withCloudSecureConnectBundle(bundle);
         };
     }
-@Autowired   FolderRepository folderRepository;
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @Autowired
+    EmailListItemRepository emailListItemRepository;
 
     @PostConstruct
     public void init() {
-		folderRepository.save(new Folder("Gupta-Vandana","sent","purple"));
-        folderRepository.save(new Folder("Gupta-Vandana","inbox","blue"));
-        folderRepository.save(new Folder("Gupta-Vandana","draft","brown"));
+        folderRepository.save(new Folder("Gupta-Vandana", "sent", "purple"));
+        folderRepository.save(new Folder("Gupta-Vandana", "inbox", "blue"));
+        folderRepository.save(new Folder("Gupta-Vandana", "draft", "brown"));
+
+        for (int i = 0; i < 10; i++) {
+            EmailListItemKey key = new EmailListItemKey();
+            key.setId("Gupta-Vandana");
+            key.setLabel("Inbox");
+            key.setTimeUUID(Uuids.timeBased());
+
+            EmailListItem emailListItem = new EmailListItem();
+            emailListItem.setKey(key);
+            emailListItem.setTo(Arrays.asList("Gupta-Vandana"));
+            emailListItem.setSubject("Subject " + i);
+            emailListItem.setUnread(true);
+
+            emailListItemRepository.save(emailListItem);
+        }
     }
 
 }
---
+++ spring-github-login-starter-master/src/main/java/main/Repository/EmailListItemRepository.java
@@ -0,0 +1,14 @@
+package main.Repository;
+
+import main.Model.EmailListItem;
+import main.Model.EmailListItemKey;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmailListItemRepository extends CassandraRepository<EmailListItem, EmailListItemKey> {
+    //List<EmailListItem> findAllById(EmailListItemKey id);
+    List<EmailListItem> findAllByKey_IdAndKey_Label(String id,String label);
+}
